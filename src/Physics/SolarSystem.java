22081ba3d7d2dfeb9e5e36f78e8a01bb7d81bb9d
MaVersch <mathias.verschueren@gmail.com>
2021-03-14 19:07:55 +0100

    Added interfaces to package titan

---
+++ src/Physics/SolarSystem.java
@@ -0,0 +1,15 @@
+package Physics;
+
+import titan.FunctionInterface;
+import titan.Vector3dInterface;
+
+
+public class SolarSystem implements FunctionInterface {
+
+    public final double GRAV_CONSTANT = 5;
+
+    @Override
+    public Vector3dInterface call(double t, Vector3dInterface s) {
+        return null;
+    }
+}
