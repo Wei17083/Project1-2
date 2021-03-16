f373646e565c497ee65e5f92f47b973eacc28407
Elena Perego <elena.perego2001@gmail.com>
2021-03-16 21:55:02 +0100

    test

--- src/titan/SolarSystem.java
+++ src/titan/SolarSystem.java
@@ -8,71 +8,65 @@ import java.util.Random;
 
 public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
 
-
         public static void main(String[] args) throws InterruptedException {
                 // radius taken from
                 // https://solarsystem.nasa.gov/solar-system
                 // other values taken from solar_system_data-2020_04_01.txt
 
-                Body sun = new Body("Sun", 1,1.988500e30,
+                Body sun = new Body("Sun", 0, 1.988500e30,
                         new Vector(-6.806783239281648e+08, 1.080005533878725e+09, 6.564012751690170e+06),
                         new Vector(-1.420511669610689e+01, -4.954714716629277e+00, 3.994237625449041e-01),
                         6.95508e8, new Color(249, 249, 127)); // yellow+white
 
-                Body mercury = new Body("Mercury", 2,3.302e23,
+                Body mercury = new Body("Mercury", 1, 3.302e23,
                         new Vector(6.047855986424127e+06, -6.801800047868888e+10, -5.702742359714534e+09),
                         new Vector(3.892585189044652e+04, 2.978342247012996e+03, -3.327964151414740e+03),
                         2.4397e6, new Color(145, 162, 171)); // grey
 
-                Body venus = new Body("Venus", 3,4.8685e24,
+                Body venus = new Body("Venus", 2, 4.8685e24,
                         new Vector(-9.435345478592035e+10, 5.350359551033670e+10, 6.131453014410347e+09),
                         new Vector(-1.726404287724406e+04, -3.073432518238123e+04, 5.741783385280979e-04),
                         6.0518e6, new Color(181, 92, 69)); // red+brown+grey
 
-                Body earth = new Body("Earth", 4,5.97219e24,
+                Body earth = new Body("Earth", 3, 5.97219e24,
                         new Vector(-1.471922101663588e+11, -2.860995816266412e+10, 8.278183193596080e+06),
                         new Vector(5.427193405797901e+03, -2.931056622265021e+04, 6.575428158157592e-01),
                         6.371e6, new Color(114, 213, 190)); // green+blue+grey
 
-                Body moon = new Body("Moon", 5,7.349e22,
+                Body moon = new Body("Moon", 4, 7.349e22,
                         new Vector(-1.472343904597218e+11, -2.822578361503422e+10, 1.052790970065631e+07),
                         new Vector(4.433121605215677e+03, -2.948453614110320e+04, 8.896598225322805e+01),
                         1.7375e6, Color.gray);
 
-                Body mars = new Body("Mars", 6,6.4171e23,
+                Body mars = new Body("Mars", 5, 6.4171e23,
                         new Vector(-3.615638921529161e+10, -2.167633037046744e+11, -3.687670305939779e+09),
                         new Vector(2.481551975121696e+04, -1.816368005464070e+03, -6.467321619018108e+02),
                         3.3895e6, new Color(171, 62, 41)); // red+brown+dark
 
-                Body jupiter = new Body("Jupiter", 7,1.89813e27,
+                Body jupiter = new Body("Jupiter", 6, 1.89813e27,
                         new Vector(1.781303138592153e+11, -7.551118436250277e+11, -8.532838524802327e+08),
                         new Vector(1.255852555185220e+04, 3.622680192790968e+03, -2.958620380112444e+02),
                         6.9911e7, new Color(215, 148, 61)); // orange+brown
 
-                Body saturn = new Body("Saturn", 8,5.6834e26,
+                Body saturn = new Body("Saturn", 7, 5.6834e26,
                         new Vector(6.328646641500651e+11, -1.358172804527507e+12, -1.578520137930810e+09),
                         new Vector(8.220842186554890e+03, 4.052137378979608e+03, -3.976224719266916e+02),
                         5.8232e7, new Color(208, 180, 40)); // gold+brown
 
-                Body titan = new Body("Titan", 9,1.34553e23,
+                Body titan = new Body("Titan", 8, 1.34553e23,
                         new Vector(6.332873118527889e+11, -1.357175556995868e+12, -2.134637041453660e+09),
                         new Vector(3.056877965721629e+03, 6.125612956428791e+03, -9.523587380845593e+02),
                         2.5755e6, new Color(163, 191, 89)); // yellow+green
 
-                Body uranus = new Body("Uranus", 10,8.6813e25,
+                Body uranus = new Body("Uranus", 9, 8.6813e25,
                         new Vector(2.395195786685187e+12, 1.744450959214586e+12, -2.455116324031639e+10),
                         new Vector(-4.059468635313243e+03, 5.187467354884825e+03, 7.182516236837899e+01),
                         2.5362e7, new Color(121, 227, 235)); // blue+green
 
-                Body neptune = new Body("Neptune", 11,1.02413e26,
+                Body neptune = new Body("Neptune", 10, 1.02413e26,
                         new Vector(4.382692942729203e+12, -9.093501655486243e+11, -8.227728929479486e+10),
                         new Vector(1.068410720964204e+03, 5.354959501569486e+03, -1.343918199987533e+02),
                         2.4622e7, Color.blue);
-
-
-
-
-
 
                 // create arrays of bodies and corresponding forces
                 Body[] bodies = new Body[] { sun, mercury, venus, earth, moon, mars, jupiter, saturn, titan, uranus,
@@ -82,7 +76,7 @@ public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
                 StdDraw.enableDoubleBuffering(); // things are only drawn on next show()
                 StdDraw.setCanvasSize(750, 750);
                 // setting up scale in order to display whole solar system
-                int scale = 10;
+                int scale = 14;
                 StdDraw.setXscale(-scale * AU, scale * AU);
                 StdDraw.setYscale(-scale * AU, scale * AU);
 
@@ -116,7 +110,7 @@ public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
                                 // set body to new pos
 
                         }
-                        earth.setPosition((Vector) pos);
+                        // earth.setPosition((Vector) pos);
 
                         StdDraw.show();
 
@@ -151,7 +145,7 @@ public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
                 this.bodies = bodies;
                 ArrayList<Vector3dInterface> positionList = new ArrayList<>();
                 ArrayList<Vector3dInterface> velocityList = new ArrayList<>();
-                for (Body b :bodies) {
+                for (Body b : bodies) {
                         positionList.add(b.getPosition());
                         velocityList.add(b.getVelocity());
                 }
@@ -162,7 +156,7 @@ public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
                 return bodies;
         }
 
-        public State getState(){
+        public State getState() {
                 return initialState;
         }
 
@@ -174,50 +168,60 @@ public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
 
         }
 
-
         /*
          * Solve the differential equation by taking multiple steps.
          *
-         * @param   f       the function defining the differential equation dy/dt=f(t,y)
-         * @param   y0      the starting state
-         * @param   ts      the times at which the states should be output, with ts[0] being the initial time
-         * @return  an array of size ts.length with all intermediate states along the path
+         * @param f the function defining the differential equation dy/dt=f(t,y)
+         *
+         * @param y0 the starting state
+         *
+         * @param ts the times at which the states should be output, with ts[0] being
+         * the initial time
+         *
+         * @return an array of size ts.length with all intermediate states along the
+         * path
          */
         @Override
         public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double[] ts) {
                 double stepSize = 1000;
-                int iterations = (int) (Math.round(ts[ts.length -1]) + 1);
+                int iterations = (int) (Math.round(ts[ts.length - 1]) + 1);
                 StateInterface[] stateList = new StateInterface[ts.length];
                 stateList[0] = y0;
                 StateInterface state = y0;
                 int index = 1;
-                for (int i = 1; i < iterations ; i++) {
-                        if (Math.abs((i-1)*stepSize - ts[index]) < Math.abs(i*stepSize - ts[index])) {
+                for (int i = 1; i < iterations; i++) {
+                        if (Math.abs((i - 1) * stepSize - ts[index]) < Math.abs(i * stepSize - ts[index])) {
                                 stateList[index] = state;
                                 index++;
                         }
-                        state = step(f, stepSize*i, state, stepSize);
+                        state = step(f, stepSize * i, state, stepSize);
                 }
                 return stateList;
         }
 
         /*
-         * Solve the differential equation by taking multiple steps of equal size, starting at time 0.
-         * The final step may have a smaller size, if the step-size does not exactly divide the solution time range
+         * Solve the differential equation by taking multiple steps of equal size,
+         * starting at time 0. The final step may have a smaller size, if the step-size
+         * does not exactly divide the solution time range
          *
-         * @param   f       the function defining the differential equation dy/dt=f(t,y)
-         * @param   y0      the starting state
-         * @param   tf      the final time
-         * @param   h       the size of step to be taken
-         * @return  an array of size round(tf/h)+1 including all intermediate states along the path
+         * @param f the function defining the differential equation dy/dt=f(t,y)
+         *
+         * @param y0 the starting state
+         *
+         * @param tf the final time
+         *
+         * @param h the size of step to be taken
+         *
+         * @return an array of size round(tf/h)+1 including all intermediate states
+         * along the path
          */
         @Override
         public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double tf, double h) {
-                int size = (int) Math.round(tf/h + 1);
+                int size = (int) Math.round(tf / h + 1);
                 StateInterface[] stateList = new StateInterface[size];
                 stateList[0] = y0;
-                for (int i = 1; i <stateList.length ; i++) {
-                        stateList[i] = step(f, h*i, stateList[i-1] ,h);
+                for (int i = 1; i < stateList.length; i++) {
+                        stateList[i] = step(f, h * i, stateList[i - 1], h);
                 }
                 return stateList;
         }
@@ -225,11 +229,15 @@ public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
         /*
          * Update rule for one step.
          *
-         * @param   f   the function defining the differential equation dy/dt=f(t,y)
-         * @param   t   the time
-         * @param   y   the state
-         * @param   h   the step size
-         * @return  the new state after taking one step
+         * @param f the function defining the differential equation dy/dt=f(t,y)
+         *
+         * @param t the time
+         *
+         * @param y the state
+         *
+         * @param h the step size
+         *
+         * @return the new state after taking one step
          */
         @Override
         public StateInterface step(ODEFunctionInterface f, double t, StateInterface y, double h) {
@@ -237,33 +245,34 @@ public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
         }
 
         /*
-         * This is an interface for the function f that represents the
-         * differential equation dy/dt = f(t,y).
-         * You need to implement this function to represent to the laws of physics.
+         * This is an interface for the function f that represents the differential
+         * equation dy/dt = f(t,y). You need to implement this function to represent to
+         * the laws of physics.
          *
-         * For example, consider the differential equation
-         *   dy[0]/dt = y[1];  dy[1]/dt=cos(t)-sin(y[0])
-         * Then this function would be
-         *   f(t,y) = (y[1],cos(t)-sin(y[0])).
+         * For example, consider the differential equation dy[0]/dt = y[1];
+         * dy[1]/dt=cos(t)-sin(y[0]) Then this function would be f(t,y) =
+         * (y[1],cos(t)-sin(y[0])).
          *
-         * @param   t   the time at which to evaluate the function
-         * @param   y   the state at which to evaluate the function
-         * @return  The average rate-of-change over the time-step. Has dimensions of [state]/[time].
+         * @param t the time at which to evaluate the function
+         *
+         * @param y the state at which to evaluate the function
+         *
+         * @return The average rate-of-change over the time-step. Has dimensions of
+         * [state]/[time].
          */
         @Override
         public RateInterface call(double t, StateInterface y) {
                 State y1 = (State) y;
                 ChangeRate rate = new ChangeRate();
-                for (Body b : bodies) {
-                        rate.addPositionChange(y1.getVelocityList().get(b.getID()));
-
+                for (int i = 0; i < bodies.length; i++) {
+                        rate.addPositionChange(y1.getVelocityList().get(bodies[i].getID()));
                         ArrayList<Vector3dInterface> forces = new ArrayList<>();
-                        for (Body b2: bodies) {
-                                if(!(b.equals(b2))){
-                                        forces.add(b2.gravitationalPull(b));
+                        for (Body b2 : bodies) {
+                                if (!(bodies[i].equals(b2))) {
+                                        forces.add(b2.gravitationalPull(bodies[i]));
                                 }
                         }
-                        rate.addVelocityChange(VectorTools.sumAll(forces).mul(1/b.getMass()));
+                        rate.addVelocityChange(VectorTools.sumAll(forces).mul(1 / bodies[i].getMass()));
                 }
                 return rate;
         }
