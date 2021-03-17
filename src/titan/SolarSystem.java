39becb7b98bb1a4de58210bdcc7defa81552e746
MaVersch <mathias.verschueren@gmail.com>
2021-03-17 16:50:37 +0100

    +1 is now +2

--- src/titan/SolarSystem.java
+++ src/titan/SolarSystem.java
@@ -136,7 +136,7 @@ public class SolarSystem implements ODESolverInterface, ODEFunctionInterface {
         @Override
         public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double[] ts) {
                 double stepSize = 100;
-                int iterations = (int) (Math.round((ts[ts.length - 1]) / stepSize) + 1);
+                int iterations = (int) (Math.round((ts[ts.length - 1]) / stepSize) + 2);
                 StateInterface[] stateList = new StateInterface[ts.length];
                 stateList[0] = y0;
                 StateInterface state = y0;
