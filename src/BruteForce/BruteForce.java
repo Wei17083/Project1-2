6394a848c39c3f57dc3d68266005bb225e3abbac
MaVersch <mathias.verschueren@gmail.com>
2021-03-18 13:20:27 +0100

    brute force getting better

--- src/BruteForce/BruteForce.java
+++ src/BruteForce/BruteForce.java
@@ -11,16 +11,21 @@ public class BruteForce {
     private static final double minSpeed = 12000;//m/s
     private static final double maxSpeed = 60000;//m/s
 
+    private static final int EARTH_ID = 3;
+    private static final int TITAN_ID = 8;
+
+    private static final double RADIUS_TITAN  = 2.5755e6;
+
     public static double getMinimum(Vector3dInterface[] trajectory, StateInterface[] statesList){
         Vector3dInterface minimum = trajectory[0];
         State initialState = (State)statesList[0];
         int step = 0;
-        double minimumDistance = minimum.dist(initialState.getPositionList().get(8));
+        double minimumDistance = minimum.dist(initialState.getPositionList().get(TITAN_ID));
         for (int i = 1; i < trajectory.length; i++) {
             State temp = (State)statesList[i-1];
-            if(trajectory[i].dist(temp.getPositionList().get(8)) < minimumDistance){
+            if(trajectory[i].dist(temp.getPositionList().get(TITAN_ID)) < minimumDistance){
                 minimum = trajectory[i];
-                minimumDistance = trajectory[i].dist(temp.getPositionList().get(8));
+                minimumDistance = trajectory[i].dist(temp.getPositionList().get(TITAN_ID));
                 step = i;
             }
         }
@@ -31,24 +36,93 @@ public class BruteForce {
     public static ArrayList<Vector3dInterface> bruteforce (SolarSystem system){
         StateInterface[] states = system.solve(system, system.getState(), 31556926, 1000);
 
+        double distanceBest = 0;
+        Vector3dInterface velocityBest = new Vector(0,0,0);
+        Vector3dInterface positionBest = new Vector(0,0,0);
+        Vector3dInterface unitVectorBest;
+        double launchSpeedBest = 0;
 
-        for(int i = 0; i < 10000; i++){
+
+        Probe spaceship = new Probe(system, states);
+
+        for(int i = 0; i < 10; i++){
             Vector3dInterface unitVector = VectorTools.randUnitVector();
             Vector3dInterface position = EarthP.addMul(EarthR, unitVector);
             double launchSpeed = Math.random()*(maxSpeed-minSpeed) + minSpeed;
             //System.out.println("Random unit vector: " + unitVector.toString());
             Vector3dInterface velocity = EarthV.addMul(launchSpeed, unitVector);
-            Probe spaceship = new Probe(system, states);
+
 
             //System.out.println("Initial speed: " + velocity.toString());
-            Vector3dInterface[] trajectories = spaceship.trajectory(position, velocity, 31556926, 1000);
-            System.out.println(getMinimum(trajectories, states));
+            Vector3dInterface[] trajectory = spaceship.trajectory(position, velocity, 31556926, 1000);
+            //System.out.println(getMinimum(trajectories, states));
 
-            if(getMinimum(trajectories, states) < 2.5755e6) {
-                System.out.println("hit");
+            if( i == 0 || getMinimum(trajectory, states) < distanceBest ) {
+                distanceBest = getMinimum(trajectory, states);
+                velocityBest = velocity;
+                positionBest = position;
+                unitVectorBest = unitVector;
+                launchSpeedBest = launchSpeed;
             }
         }
 
+        double initialDistanceET = system.getState().getPositionList().get(EARTH_ID).dist(system.getState().getPositionList().get(TITAN_ID));
+
+        System.out.println("Starting while loop");
+        boolean hit = false;
+        while(!hit) {
+            System.out.println();
+
+            double randInt = Math.random();
+            int randPower = (int) (Math.random()*2 + 1);
+            double changeRate = Math.random()*5*(distanceBest/initialDistanceET)*Math.pow(-1, randPower);
+            System.out.println("ChangeRate: " + changeRate);
+
+
+            Vector3dInterface unitVector;
+            double launchSpeedNew = velocityBest.norm();
+
+            if(randInt < 0.25) {
+                double newX = velocityBest.getX() + velocityBest.getX()*changeRate;
+                unitVector = VectorTools.getUnitVector(new Vector(newX, velocityBest.getY(), velocityBest.getZ()));
+            } else if(randInt < 0.5) {
+                double newY = velocityBest.getY() + velocityBest.getY()*changeRate;
+                unitVector = VectorTools.getUnitVector(new Vector(velocityBest.getX(), newY , velocityBest.getZ()));
+            } else if (randInt < 0.75){
+                double newZ = velocityBest.getZ() + velocityBest.getZ()*changeRate;
+                unitVector = VectorTools.getUnitVector(new Vector(velocityBest.getX(), velocityBest.getY(), newZ));
+            } else {
+                launchSpeedNew = velocityBest.norm() + velocityBest.norm()*changeRate;
+                if(launchSpeedNew > maxSpeed) launchSpeedNew = maxSpeed;
+                unitVector = VectorTools.getUnitVector(velocityBest);
+            }
+            Vector3dInterface position = EarthP.addMul(EarthR, unitVector);
+            Vector3dInterface velocity = EarthV.addMul(launchSpeedNew, unitVector);
+
+            Vector3dInterface[] trajectory = spaceship.trajectory(position, velocity, 31556926, 1000);
+
+            double shortestDistance = getMinimum(trajectory, states);
+            System.out.println("shortest distance: " + shortestDistance);
+            System.out.println("shortest so far: " + distanceBest);
+            if(  shortestDistance < distanceBest ) {
+                distanceBest = shortestDistance;
+                velocityBest = velocity;
+                positionBest = position;
+                unitVectorBest = unitVector;
+                launchSpeedBest = launchSpeedNew;
+                System.out.println(distanceBest);
+            }
+
+            if(shortestDistance < RADIUS_TITAN) {
+                System.out.println("OH MY GOD WE HIT IT");
+                System.out.println(velocityBest.toString());
+                System.out.println(positionBest.toString());
+                hit = true;
+            }
+
+
+        }
+        //2.5755e6
         return new ArrayList<Vector3dInterface>();
     }
 }
