38662d3cffd4baef629117c9e215a17becaf5cec
Elena Perego <elena.perego2001@gmail.com>
2021-04-25 18:49:44 +0200

    add junit tests

---
+++ test/titan/Physics/ChangeRateTest.java
@@ -0,0 +1,27 @@
+package titan.Physics;
+
+import org.junit.jupiter.api.Test;
+import titan.ChangeRate;
+import titan.Vector;
+import titan.Vector3dInterface;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChangeRateTest {
+    @Test
+    void getSet(){
+        Vector p1 = new Vector(2, 2, 2);
+        Vector v1 = new Vector(4, 4, 4);
+        ArrayList<Vector3dInterface> positionChanges = new ArrayList<>();
+        ArrayList<Vector3dInterface> velocityChanges = new ArrayList<>();
+        positionChanges.add(p1);
+        velocityChanges.add(v1);
+        ChangeRate c = new ChangeRate();
+        c.addPositionChange(p1);
+        c.addVelocityChange(v1);
+        assertEquals(c.getPositionChanges(), positionChanges);
+        assertEquals(c.getVelocityChanges(), velocityChanges);
+    }
+}
