43445f3ad061b193370a79ddcdaf66ae9ec058f0
MaVersch <mathias.verschueren@gmail.com>
2021-05-18 19:46:53 +0200

    updated some methods

--- src/NewtonRhapson/NewtonRhapson.java
+++ src/NewtonRhapson/NewtonRhapson.java
@@ -1,7 +1,11 @@
 package NewtonRhapson;
 
+import Probe.ProbeSimulator;
 import titan.State;
+import titan.Vector;
 import titan.Vector3dInterface;
+
+import javax.swing.*;
 
 public class NewtonRhapson {
 
@@ -25,5 +29,46 @@ public class NewtonRhapson {
             //invert jacobian matrix
 
         //Find desired initial velocity
+    }
+
+    /**
+     *  Will calculate the partial derivative of var1 to var 2
+     * @param var1  first variable, will be slightly changed
+     * @param var2
+     * @param initialVelocity velocity to calculate the derivative
+     * @return
+     */
+    public double getPartialDerivative(char var1, char var2, Vector3dInterface initialPosition, Vector3dInterface initialVelocity){
+        double velocityComponent1 = getVelocityComponent(var1, initialVelocity);
+        double velocityComponent2 = getVelocityComponent(var2, initialVelocity);
+
+        double relativeChange = 0.0001;
+        double change = Math.abs(velocityComponent1*relativeChange);
+
+        ProbeSimulator probe = new ProbeSimulator();
+        Vector3dInterface[] trajectoryPlus = probe.trajectoryAbsoluteInitialVelocity(initialPosition,initialVelocity, tf, h);
+        Vector3dInterface[] trajectoryMinus =
+        double valuePositiveChange =
+        double valueNegativechange =
+    }
+
+    private double getVelocityComponent(char var, Vector3dInterface initialVelocity) {
+        double velocityComponent = 0;
+        switch (var) {
+            case ('x') -> velocityComponent = initialVelocity.getX();
+            case ('y') -> velocityComponent = initialVelocity.getY();
+            case ('z') -> velocityComponent = initialVelocity.getZ();
+        }
+        return velocityComponent;
+    }
+
+    private Vector3dInterface changeVelocity(char var, double change, Vector3dInterface velocity){
+        Vector3dInterface returnVelocity = new Vector();
+        switch (var){
+            case('x') -> returnVelocity = new Vector(velocity.getX() + change,velocity.getY(), velocity.getZ() );
+            case('y') -> returnVelocity = new Vector(velocity.getX(), velocity.getY() + change, velocity.getZ() );
+            case('z') -> returnVelocity = new Vector(velocity.getX(),velocity.getY(),velocity.getZ() + change );
+        }
+        return returnVelocity;
     }
 }
