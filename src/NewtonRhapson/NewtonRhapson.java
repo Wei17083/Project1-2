26cb261b7d10b643c0da1d21bd7dea1f5d651888
MaVersch <mathias.verschueren@gmail.com>
2021-05-18 20:45:37 +0200

    bleeeeeh

--- src/NewtonRhapson/NewtonRhapson.java
+++ src/NewtonRhapson/NewtonRhapson.java
@@ -5,9 +5,8 @@ import titan.State;
 import titan.Vector;
 import titan.Vector3dInterface;
 
-import javax.swing.*;
-
 public class NewtonRhapson {
+
 
     public Vector3dInterface findInitialVelocity(State initialState, Vector3dInterface finalPosition, double stepSize, double finalTime) {
 
@@ -39,20 +38,28 @@ public class NewtonRhapson {
      * @return
      */
     public double getPartialDerivative(char var1, char var2, Vector3dInterface initialPosition, Vector3dInterface initialVelocity){
-        double velocityComponent1 = getVelocityComponent(var1, initialVelocity);
-        double velocityComponent2 = getVelocityComponent(var2, initialVelocity);
+        double velocityComponent1 = getVectorComponent(var1, initialVelocity);
+        double velocityComponent2 = getVectorComponent(var2, initialVelocity);
 
         double relativeChange = 0.0001;
         double change = Math.abs(velocityComponent1*relativeChange);
 
         ProbeSimulator probe = new ProbeSimulator();
-        Vector3dInterface[] trajectoryPlus = probe.trajectoryAbsoluteInitialVelocity(initialPosition,initialVelocity, tf, h);
-        Vector3dInterface[] trajectoryMinus =
-        double valuePositiveChange =
-        double valueNegativechange =
+
+        Vector3dInterface velocityPositiveChange = changeVelocity(var1, change, initialVelocity);
+        Vector3dInterface velocityNegativeChange = changeVelocity(var1, -change, initialVelocity);
+
+        Vector3dInterface[] trajectoryPlus = probe.trajectoryAbsoluteInitialVelocity(initialPosition,velocityPositiveChange, tf, h);
+        Vector3dInterface[] trajectoryMinus = probe.trajectoryAbsoluteInitialVelocity(initialPosition, velocityNegativeChange, tf, h);
+
+        Vector3dInterface directionVectorPlus = calculateDistanceFromDestination(trajectoryPlus);
+        Vector3dInterface directionVectorMinus = calculateDistanceFromDestination(trajectoryMinus);
+
+        double derivative = (getVectorComponent(var2, directionVectorPlus)-getVectorComponent(var2, directionVectorMinus)/2*change);
+        return derivative;
     }
 
-    private double getVelocityComponent(char var, Vector3dInterface initialVelocity) {
+    private double getVectorComponent(char var, Vector3dInterface initialVelocity) {
         double velocityComponent = 0;
         switch (var) {
             case ('x') -> velocityComponent = initialVelocity.getX();
