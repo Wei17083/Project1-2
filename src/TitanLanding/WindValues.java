7bc08852965bb293f38fe31256ab140ff98acb8f
Eric Weindorfer <wei17083@spengergasse.at>
2021-06-11 11:34:24 +0200

    added getSpeed method to WindValues

--- src/TitanLanding/WindValues.java
+++ src/TitanLanding/WindValues.java
@@ -26,4 +26,16 @@ public class WindValues {
     public void setSpeeds(Vector3dInterface[] speeds) {
         this.speeds = speeds;
     }
+
+    /**
+     * this method gets the windspeed at the given altitude
+     * @param altitude the altitude at which we want to get the speed (in m)
+     * @return a vector with the windspeed in m/s
+     */
+    public Vector3dInterface getWindSpeed(double altitude){
+        for (int i = 0; i < altitudes.length; i++) {
+            if(altitude>altitudes[i]) return speeds[i];
+        }
+        return null;
+    }
 }
