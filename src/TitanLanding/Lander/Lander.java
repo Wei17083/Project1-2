6d0c2581f2c2176a936bb18b0aebcb3cdf371c15
Collin2210 <makuzacollin@gmail.com>
2021-06-13 14:07:49 +0200

    commit message

--- src/TitanLanding/Lander/Lander.java
+++ src/TitanLanding/Lander/Lander.java
@@ -1,4 +1,14 @@
 package TitanLanding.Lander;
 
+import titan.*;
+
 public class Lander {
+    private final double RADIUS = 4; // 4 meters
+    private final double MASS = 6000; // 6000 kg
+    public LanderState state;
+
+    public Lander(LanderState ogState){
+        this.state = ogState;
+    }
+
 }
